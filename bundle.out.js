2d668fbadbb7cb9258653fc8f91efe67466d4e08
hikire <hikiML.re@gmail.com>
2019-01-23 22:16:11 +0200

    prettify previous output

--- bundle.out.js
+++ bundle.out.js
@@ -1,50 +1,55 @@
+var process = { env: { NODE_ENV: "development" } };
+(function(modules) {
+  function require(id) {
+    var [fn, mapping] = modules[id];
+    function localRequire(localFileName) {
+      return require(mapping[localFileName]);
+    }
+    var module = { exports: {} };
+    fn(localRequire, module, module.exports);
+    return module.exports;
+  }
 
-    var process = {env:{"NODE_ENV":"development"}};
-    (function(modules){
-      function require(id) {
-        var [fn, mapping] = modules[id];
-        function localRequire(localFileName) {
-          return require(mapping[localFileName]);
-        }
-        var module = {exports:{}};
-        fn(localRequire, module, module.exports);
-        return module.exports;
-      }
-
-      require(0);
-    })({
-    0 : [function(require, module, exports){
+  require(0);
+})({
+  0: [
+    function(require, module, exports) {
       "use strict";
 
-const log = require("./logger.js").default;
+      const log = require("./logger.js").default;
 
-log();
-    }, {"./logger.js":1}]
-  ,
-    1 : [function(require, module, exports){
+      log();
+    },
+    { "./logger.js": 1 }
+  ],
+  1: [
+    function(require, module, exports) {
       "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+      Object.defineProperty(exports, "__esModule", {
+        value: true
+      });
+      exports.default = void 0;
+
+      var _message = require("./message.js");
+
+      var _default = () => console.log(_message.message);
+
+      exports.default = _default;
+    },
+    { "./message.js": 2 }
+  ],
+  2: [
+    function(require, module, exports) {
+      "use strict";
+
+      Object.defineProperty(exports, "__esModule", {
+        value: true
+      });
+      exports.message = void 0;
+      const message = "Yo :\\";
+      exports.message = message;
+    },
+    {}
+  ]
 });
-exports.default = void 0;
-
-var _message = require("./message.js");
-
-var _default = () => console.log(_message.message);
-
-exports.default = _default;
-    }, {"./message.js":2}]
-  ,
-    2 : [function(require, module, exports){
-      "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.message = void 0;
-const message = "Yo :\\";
-exports.message = message;
-    }, {}]
-  })
-  
